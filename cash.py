57aaac4906f254dbce8dbd761a19c7f6948b3c99
jtopen <jtopen@users.noreply.github.com>
2018-05-31 16:26:02 +0000

    20180531T162526Z

---
+++ cash.py
@@ -0,0 +1,31 @@
+#gives number of coins (25c 10c 5c 1c) to make a float input
+import cs50
+
+#get positive float input
+while True:
+    change = cs50.get_float("How much change?")
+    if change > 0:
+        break
+
+#input in cents, rounded to nearest cent
+cents = round(100 * change)
+
+#declare counter (# of coins)
+count = 0
+
+#quarters, floor-divide by 25 for count, remainder is what's left!
+count += (cents // 25)
+cents = cents % 25
+
+#dimes
+count += (cents // 10)
+cents = cents % 10
+
+#nickels
+count += (cents // 5)
+cents = cents % 5
+
+#pennies, no need to divide here.
+count += cents
+
+print(f"count is {count}")
