140afa8e2e4f25b98811b3ba2b6869efa9d24acf
jtopen <jtopen@users.noreply.github.com>
2018-06-19 19:15:40 +0000

    20180619T191507Z

--- recover.c
+++ recover.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(argc, argv)
+int main(int argc, char *argv[])
 {
-
     //check command line args
     while (argc != 2)
     {
-        //fprintf to stderr
+        //fprintf to stderr if incorrect input
         fprintf(stderr, "please enter ./recover [filename]");
         return 1;
     }
@@ -17,59 +16,66 @@ int main(argc, argv)
 
     // open input file
     FILE *inptr = fopen(infile, "r");
+
+    //check if input file was opened (if inptr points to infile or NULL)
     if (inptr == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
 
+    //define image buffer (array of BYTE) for holding 512 byte blocks
+    typedef uint8_t BYTE;
+    BYTE buff[512];
+    int buffer_size = sizeof(buff);
+
     //initialize image name counter
     int image_number = 0;
 
-    //define image buffer for searching 512byte blocks
-    typedef uint8_t BYTE;
-    BYTE buffer = [512];
-    buffer_size = sizeof(buffer);
+    //initialize filename
+    char iterative_filename[8];
 
-    //while there's still image to search, keep searching
-    while(inptr / !EOF)
+    FILE *image_write = NULL;
+
+    do //reads from infile to buff, checks buff for jpeg header
     {
+        fread(buff, buffer_size, 1, inptr);
 
-    //open new outfile with name 000.jpeg - xxx.jpeg
+    }
+    while (buff[0] != 0xff && buff[1] != 0xd8 &&
+           buff[2] != 0xff && (buff[3] & 0xf0) != 0xe0);
 
-        iterative_name = fprintf("%03d.jpeg",image_number);
+    while (image_number == 0) //writes first block to first jpeg
+    {
+        //open new outfile with name 000.jpeg - xxx.jpeg
+        sprintf(iterative_filename, "%03d.jpg", image_number);
+        image_write = fopen(iterative_filename, "w");
+        fwrite(buff, buffer_size, 1, image_write);//TODO check fwrite
+        image_number ++;
+    }
 
-        fopen(iterative_name, "w");
-
-        fwrite(inptr, buffer_size, 512, iterative_name);
-
-        while(writing to outfile, until end of jpeg write)
-        //iterate over blocks / read a 'check block' to determine whether it's a jpeg
-
-        fread(inptr, 512, , );
-
-        //test for jpeg--should return true
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 &&
-            buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-
-
-        // if no, keep going
-        while (current block does not start with 0xff 0xd8 0xff)
+    //loop after start of jpegs has been found, repeats while there buffer_size left in infile
+    while (fread(buff, buffer_size, 1, inptr) == 1)
+    {
+        //if the current block is the start of a jpeg
+        if (buff[0] == 0xff && buff[1] == 0xd8 &&
+            buff[2] == 0xff && (buff[3] & 0xf0) == 0xe0)
         {
-            keep going
+            //close the previous file, iterate filename, open file with new filename
+            //write buff to new file, iterate image number for new filename
+            fclose(image_write);
+            sprintf(iterative_filename, "%03d.jpg", image_number);
+            image_write = fopen(iterative_filename, "w");
+            fwrite(buff, buffer_size, 1, image_write);
+            image_number ++;
         }
 
-        // if block starts jpeg, start writing to outfile
+        else //while infile is still within a jpeg, write to outfile
+        {
+            fwrite(buff, buffer_size, 1, image_write);
+        }
+    }
 
-        fwrite(outfile, sizeof(BYTE), 512, );
-
-
-        //keep checking infile, one 'check block' at a time
-
-        //when finding jpeg start, end writing to outfile, and start a new outfile
-
-}
-
-
-
+    fclose(image_write); //close current jpeg
+    fclose(inptr); //close infile
 }
